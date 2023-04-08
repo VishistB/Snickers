0ab8f1a077a0b2da49661ddac36eb5b33bdc9d2a
sanya-mahajan <sanyamahajan08@gmail.com>
2023-04-08 21:51:49 +0530

    feedback

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Vidcall from './components/Videocall/vidcall';
+import Feedback from './components/Feedback/feedback';
 
 function App() {
   return (
     <div className="App">
       <Vidcall/>  
+      <Feedback/>
     </div>
   );
 }
