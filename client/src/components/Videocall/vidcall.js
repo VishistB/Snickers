1b8435d10a402da3f2681b600fd71dcffbf284c1
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-04-08 21:03:35 +0530

    vidcall component changes

--- client/src/components/Videocall/vidcall.js
+++ client/src/components/Videocall/vidcall.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import styles from './vidcall.module.css'
+import AgoraRTC from 'agora-rtc-sdk-ng'
+
+const APP_ID='7d2f6b401f8345ea9b0106cd9907839f'
+const TOKEN=''
+const CHANNEL='StudyVerse'
+
+const client=AgoraRTC.createClient({
+    mode:'rtc',
+    codec:'vp8'
+})
+
 
 function Vidcall() {
     return (
