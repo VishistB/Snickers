554d02d9c191960f0ffe1444fc9f1ac33e209900
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-04-08 20:41:08 +0530

    added components folder to client

---
+++ client/src/components/Videocall/vidcall.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './vidcall.module.css'
+
+function Vidcall() {
+    return (
+        <>
+            <div className={styles.vidcallwrap}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Vidcall
