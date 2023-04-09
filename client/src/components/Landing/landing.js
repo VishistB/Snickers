3f56441393250d7f381040b554a46a96f6a4559b
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-04-09 16:22:28 +0530

    landing page

---
+++ client/src/components/Landing/landing.js
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./Landingstyle.module.css";
+import logo from "../../assets/LEARNIFY_LOGO.png";
+import { Link } from "react-router-dom";
+import { FaHome, FaUserFriends, FaBook, FaPuzzlePiece, FaEnvelope } from 'react-icons/fa';
+import backgroundImage from '../../assets/13_1.png';
+function Landing() {
+    return (
+        <div className={styles.landingwrap} style={{backgroundImage: `url(${backgroundImage})`, backgroundSize:"cover" }}>
+            <div className={styles.tempnav} >
+                <img src={logo} style={{ width: "50px", height: "50px" }} />
+                <ul className={styles.navbarnav}>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>
+                            <Link to="/">
+                                <FaHome className={styles.navicon} />
+                            </Link>
+                            <span className={styles.linktext}></span>
+                        </a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>
+                            <Link to="/StudySession">
+                                <FaUserFriends className={styles.navicon} />
+                            </Link>
+                            <span className={styles.linktext}></span>
+                        </a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>
+                            <FaBook className={styles.navicon} />
+                            <span className={styles.linktext}></span>
+                        </a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>
+                            <FaPuzzlePiece className={styles.navicon} />
+                            <span className={styles.linktext}></span>
+                        </a>
+                    </li>
+                    <li className={styles.navitem}>
+                        <a href="#" className={styles.navlink}>
+                            <FaEnvelope className={styles.navicon} />
+                            <span className={styles.linktext}></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className={styles.mainlandingwrap}>
+                <div className={styles.mainheaders}>
+                <h1 className={styles.massivetext}>LEARNIFY</h1>
+                <h1 className={styles.submassivetext}>REVOLUTIONISING EDUCATION</h1>
+                <button className={styles.loginbtn}>SIGN UP</button>
+                </div>
+            </div>
+            <div className={styles.secondlandingwrap}>
+                <div className={styles.darkwrap}>
+                    <h2 className={styles.abtushead}>ABOUT US</h2>
+                    <div className={styles.yellowcnt}>
+                        <p></p>
+                    </div>
+                    <h2 className={styles.abtushead}>WHAT WE OFFER?</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
