99e17be4489eb9267f3a9033cf745df7677b00b8
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-04-17 00:26:47 +0530

    multiple users on videocall

--- client/src/components/Videocall/obsoletebackup.md
+++ client/src/components/Videocall/obsoletebackup.md
@@ -1,7 +1,154 @@
-<div className={styles.counterwrap}>
-                {/* {remoteUsers.map((user) => (
-                    <div key={user.uid}>
-                        <p>Remote User {user.uid}</p>
-                        <div ref={(v) => v && user.videoTrack.play(v)} />
+import React, { useState, useEffect } from "react";
+import AgoraRTC from "agora-rtc-sdk-ng";
+import styles from "./vidcall.module.css";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
+import backgroundImage from "../../assets/13_1.png";
+
+const session_duration = 6000; //millisecs
+
+let timePast = parseInt(localStorage.getItem("timePast"));
+if (!timePast) {
+    timePast = 0;
+}
+
+const appId = "7d2f6b401f8345ea9b0106cd9907839f";
+const channelName = "StudyVerse";
+const token =
+    "007eJxTYJigursyV9DSLKJM+ifvNfsz0Qo2T1vU9kXcq2d41ifQnqrAYJ5ilGaWZGJgmGZhbGKammiZZGBoYJacYmlpYG5hbJm2YbF1SkMgI0P0fm0GRigE8bkYgktKUyrDUouKUxkYAFecH4M=";
+
+const client = AgoraRTC.createClient({ codec: "h264", mode: "rtc" });
+const localTracksState = { videoTrack: null, audioTrack: null };
+
+const Vidcall = () => {
+    const [remoteUsers, setRemoteUsers] = useState([]);
+    const [remoteTracks, setRemoteTracks] = useState([]);
+    const [localTracks, setLocalTracks] = useState(localTracksState);
+
+    const [isSessionOver, setIsSessionOver] = useState(false);
+    const navigate = () => {
+        window.location.href = "/Feedback";
+        localStorage.removeItem("mytime");
+    };
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const tracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+                setLocalTracks({
+                    videoTrack: tracks[1],
+                    audioTrack: tracks[0],
+                });
+                await client.join(appId, channelName, token, null);
+                await client.publish([tracks[1], tracks[0]]);
+
+                client.on("user-published", async (user, mediaType) => {
+                    await client.subscribe(user, mediaType);
+                    if (mediaType === "video") {
+                        const remoteVideoTrack = user.videoTrack;
+                        setRemoteTracks((prevRemoteTracks) => [
+                            ...prevRemoteTracks,
+                            remoteVideoTrack,
+                        ]);
+                        setRemoteUsers((prevRemoteUsers) => [
+                            ...prevRemoteUsers,
+                            user,
+                        ]);
+                    }
+                });
+                client.on("user-unpublished", (user) => {
+                    setRemoteUsers((prevRemoteUsers) =>
+                        prevRemoteUsers.filter((u) => u !== user)
+                    );
+                });
+                if (isSessionOver) {
+                    localTracks.videoTrack.stop();
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+        return () => {
+            client.leave();
+        };
+    }, []);
+
+    const timerTick = () => {
+        timePast += 1000;
+        localStorage.setItem("timePast", timePast);
+    };
+    const timeendtrig = () => {
+        setIsSessionOver(true);
+        // stop the video when session is over
+    };
+    useEffect(() => {
+        const handleVisibilityChange = () => {
+            if (document.visibilityState === "visible") {
+                // alert("Tab switched Let's not get distracted shall we?");
+            }
+        };
+
+        document.addEventListener("visibilitychange", handleVisibilityChange);
+
+        return () => {
+            document.removeEventListener(
+                "visibilitychange",
+                handleVisibilityChange
+            );
+        };
+    }, []);
+
+    return (
+        <div
+            className={styles.vidcallwrap}
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: "cover",
+            }}
+        >
+            <div className={styles.sessionwrap}>
+                <p>SESSION LIVE</p>
+                {localTracks.videoTrack ? (
+                    <div className={styles.videoboxwrap}>
+                        <div className={styles.outervidscreen}>
+                            <div
+                                className={styles.uservidscreen}
+                                ref={(v) => v && localTracks.videoTrack.play(v)}
+                            />
+                        </div>
+                        {remoteUsers.map((user) => {
+                            const uid = user.uid;
+                            return (
+                                <div
+                                    className={styles.uservidscreen}
+                                    key={uid}
+                                    ref={(v) => user.videoTrack.play(v)}
+                                />
+                            );
+                        })}
                     </div>
-                ))} */}
+                ) : (
+                    <p>This will just take a moment..</p>
+                )}
+            </div>
+            <div className={styles.counterwrap}>
+                <div className={styles.counter}>
+                    <Countdown
+                        date={Date.now() + session_duration - timePast}
+                        onTick={timerTick}
+                        onComplete={() => {
+                            // navigate();
+                            // Handle session completion here
+                        }}
+                        // onStart={}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Vidcall;
+
+
+
--- client/src/components/Videocall/vidcall.js
+++ client/src/components/Videocall/vidcall.js
@@ -3,7 +3,8 @@ import AgoraRTC from "agora-rtc-sdk-ng";
 import styles from "./vidcall.module.css";
 import ReactDOM from "react-dom";
 import Countdown from "react-countdown";
-import backgroundImage from '../../assets/13_1.png';
+import { VideoPlayer } from "./vidplayer";
+import backgroundImage from "../../assets/13_1.png";
 
 const session_duration = 6000; //millisecs
 
@@ -12,59 +13,76 @@ if (!timePast) {
     timePast = 0;
 }
 
-const appId = "7d2f6b401f8345ea9b0106cd9907839f";
-const channelName = "StudyVerse";
-const token =
-    "007eJxTYAhWPrd8iQab9/+w4pIe0Rf2drvP9ezcIFvZN+NWWYGbXpsCg3mKUZpZkomBYZqFsYlpaqJlkoGhgVlyiqWlgbmFsWXae1eLlIZARoZDGpNZGBkgEMTnYgguKU2pDEstKk5lYAAA3BchDA==";
+const APP_ID = "7d2f6b401f8345ea9b0106cd9907839f";
+const CHANNEL = "StudyVerse";
+const TOKEN =
+    "007eJxTYJigursyV9DSLKJM+ifvNfsz0Qo2T1vU9kXcq2d41ifQnqrAYJ5ilGaWZGJgmGZhbGKammiZZGBoYJacYmlpYG5hbJm2YbF1SkMgI0P0fm0GRigE8bkYgktKUyrDUouKUxkYAFecH4M=";
 
 const client = AgoraRTC.createClient({ codec: "h264", mode: "rtc" });
 const localTracksState = { videoTrack: null, audioTrack: null };
 
 const Vidcall = () => {
-    const [remoteUsers, setRemoteUsers] = useState([]);
-    const [localTracks, setLocalTracks] = useState(localTracksState);
+    const [users, setUsers] = useState([]);
+    const [localTracks, setLocalTracks] = useState([]);
 
+    const handleUserJoined = async (user, mediaType) => {
+        await client.subscribe(user, mediaType);
+
+        if (mediaType === "video") {
+            setUsers((previousUsers) => [...previousUsers, user]);
+        }
+
+        if (mediaType === "audio") {
+            // user.audioTrack.play()
+        }
+    };
+
+    const handleUserLeft = (user) => {
+        setUsers((previousUsers) =>
+            previousUsers.filter((u) => u.uid !== user.uid)
+        );
+    };
+
+    useEffect(() => {
+        client.on("user-published", handleUserJoined);
+        client.on("user-left", handleUserLeft);
+
+        client
+            .join(APP_ID, CHANNEL, TOKEN, null)
+            .then((uid) =>
+                Promise.all([AgoraRTC.createMicrophoneAndCameraTracks(), uid])
+            )
+            .then(([tracks, uid]) => {
+                const [audioTrack, videoTrack] = tracks;
+                setLocalTracks(tracks);
+                setUsers((previousUsers) => [
+                    ...previousUsers,
+                    {
+                        uid,
+                        videoTrack,
+                        audioTrack,
+                    },
+                ]);
+                client.publish(tracks);
+            });
+
+        return () => {
+            for (let localTrack of localTracks) {
+                localTrack.stop();
+                localTrack.close();
+            }
+            client.off("user-published", handleUserJoined);
+            client.off("user-left", handleUserLeft);
+            // client.unpublish(tracks).then(() => client.leave());
+        };
+    }, []);
+
+    // STOPWATCH RELATED FUNCTIONS AND VARIABLES
     const [isSessionOver, setIsSessionOver] = useState(false);
     const navigate = () => {
         window.location.href = "/Feedback";
         localStorage.removeItem("mytime");
     };
-
-    useEffect(() => {
-        (async () => {
-            try {
-                const tracks = await AgoraRTC.createMicrophoneAndCameraTracks();
-                setLocalTracks({
-                    videoTrack: tracks[1],
-                    audioTrack: tracks[0],
-                });
-                await client.join(appId, channelName, token, null);
-                await client.publish([tracks[1], tracks[0]]);
-                client.on("user-published", async (user, mediaType) => {
-                    await client.subscribe(user, mediaType);
-                    if (mediaType === "video") {
-                        setRemoteUsers((prevRemoteUsers) => [
-                            ...prevRemoteUsers,
-                            user,
-                        ]);
-                    }
-                });
-                client.on("user-unpublished", (user) => {
-                    setRemoteUsers((prevRemoteUsers) =>
-                        prevRemoteUsers.filter((u) => u !== user)
-                    );
-                });
-                if (isSessionOver) {
-                    localTracks.videoTrack.stop();
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        })();
-        return () => {
-            client.leave();
-        };
-    }, []);
 
     const timerTick = () => {
         timePast += 1000;
@@ -74,52 +92,34 @@ const Vidcall = () => {
         setIsSessionOver(true);
         // stop the video when session is over
     };
-    useEffect(() => {
-        const handleVisibilityChange = () => {
-            if (document.visibilityState === "visible") {
-                // alert("Tab switched Let's not get distracted shall we?");
-            }
-        };
-
-        document.addEventListener("visibilitychange", handleVisibilityChange);
-
-        return () => {
-            document.removeEventListener(
-                "visibilitychange",
-                handleVisibilityChange
-            );
-        };
-    }, []);
 
     return (
-        <div className={styles.vidcallwrap}
-        style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundSize: "cover",
-        }}
+        <div
+            className={styles.vidcallwrap}
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: "cover",
+            }}
         >
-            <div className={styles.sessionwrap}>
-                <p>SESSION LIVE</p>
-                {localTracks.videoTrack ? (
-                    <div className={styles.videoboxwrap}>
-                        <div className={styles.outervidscreen}>
-                            <div
-                                className={styles.uservidscreen}
-                                ref={(v) => v && localTracks.videoTrack.play(v)}
-                            />
-                        </div>
-                    </div>
-                ) : (
-                    <p>This will just take a moment..</p>
-                )}
+            <div className={styles.videoboxwrap}>
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(3, 33%)",
+                        margin:"10px"
+                    }}
+                >
+                    {users.map((user) => (
+                        <VideoPlayer key={user.uid} user={user} />
+                    ))}
+                </div>
             </div>
             <div className={styles.counterwrap}>
                 <div className={styles.counter}>
-                    <Countdown 
-                        date={Date.now() + session_duration  - timePast}
+                    <Countdown
+                        date={Date.now() + session_duration - timePast}
                         onTick={timerTick}
                         onComplete={() => {
-                        
                             // navigate();
                             // Handle session completion here
                         }}
